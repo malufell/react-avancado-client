a4fa1cae1d99d9924355538e0880ed3a004a7b15
Malu <malufell@hotmail.com>
2021-03-25 19:59:13 -0300

    implementa componente Logo

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'], //caminho onde estarão meus arquivos de teste
   testEnvironment: 'jsdom', //simula dom do browser pra permitir que os testes rodem direitinho
   testPathIgnorePatterns: ['/node_modules/', '/.next/'], //informa as pastas que ele deve ignorar
   collectCoverage: true, //vai fazer o teste de cobertura pra ver se estamos cobrindo todas funções, variáveis, etc
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'], //informando todos os arquivos do projeto com código e desconsiderando stories dos testes
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts'
+  ], //informando todos os arquivos do projeto com código e desconsiderando stories dos testes
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'] //configurações que o jest deve carregar antes de implementar os testes
 }
