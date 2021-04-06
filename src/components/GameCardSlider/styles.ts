c8825fe81fd64ed84089be0e1191726694a4b016
Malu <malufell@hotmail.com>
2021-04-06 17:08:24 -0300

    implementa componente GameCardSlider

---
+++ src/components/GameCardSlider/styles.ts
@@ -0,0 +1,60 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+import { GameCardSliderProps } from '.'
+
+type WrapperProps = Pick<GameCardSliderProps, 'color'>
+
+export const Wrapper = styled.section<WrapperProps>`
+  ${({ theme, color }) => css`
+    ${media.lessThan('huge')`
+    //impede que apareça o scroll
+      overflow-x: hidden;
+    `}
+    .slick-track,
+    .slick-list {
+      display: flex;
+    }
+    .slick-slide > div {
+      margin: 0 ${theme.spacings.xxsmall};
+      //deixa todos os cards com a mesma altura
+      flex: 1 0 auto;
+      height: 100%;
+    }
+    .slick-list {
+      margin: 0 -${theme.spacings.xxsmall};
+    }
+    ${media.greaterThan('large')`
+      .slick-slide > div {
+        margin: 0 ${theme.spacings.xsmall};
+      }
+      .slick-list {
+        margin: 0 -${theme.spacings.xsmall};
+      }
+    `}
+    .slick-prev,
+    .slick-next {
+      display: block;
+      color: ${theme.colors[color!]};
+      cursor: pointer;
+      position: absolute;
+      top: 50%;
+      width: 2.5rem;
+      height: 2.5rem;
+      padding: 0;
+      transform: translate(0, -50%);
+    }
+    //posiciona as setas, valor é negativo pra ficarem fora dos slides
+    .slick-prev {
+      left: -${theme.spacings.xxlarge};
+    }
+    .slick-next {
+      right: -${theme.spacings.xxlarge};
+    }
+    //esconde a seta se não tiver mais itens
+    .slick-prev.slick-disabled,
+    .slick-next.slick-disabled {
+      visibility: hidden;
+    }
+  `}
+`
